adfbfd847bf110519dbabd71e05a8e6b1cf1fc94
Ezekiel Abodesegun <ezekielphlat@gmail.com>
2022-11-05 23:19:47 +0100

    add stores  reducers subscription and dispatch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addUser } from "./features/Users";
 
 function App() {
+  const dispatch = useDispatch();
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const userList = useSelector((state) => state.users.value);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="addUser">
+        <input
+          type="text"
+          value={name}
+          placeholder="Name...."
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          value={username}
+          placeholder="Username...."
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            dispatch(addUser({ id: userList.length, name, username }));
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Add User
+        </button>
+      </div>
+      <div className="displayUsers">
+        {userList.map((user) => (
+          <div>
+            <h1>{user.name}</h1>
+            <h1>{user.username}</h1>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/FakeData.js
@@ -0,0 +1,42 @@
+export const UsersData = [
+  {
+    id: 1,
+    name: "Ezekiel Abodesegun",
+    username: "eabodesegun",
+  },
+  {
+    id: 2,
+    name: "Nicky Kage",
+    username: "nkage",
+  },
+  {
+    id: 3,
+    name: "Sarah Cream",
+    username: "scream",
+  },
+  {
+    id: 4,
+    name: "Cole Man",
+    username: "coleman",
+  },
+  {
+    id: 5,
+    name: "Daniel Emeka",
+    username: "demeka",
+  },
+  {
+    id: 6,
+    name: "John Dosh",
+    username: "foo",
+  },
+  {
+    id: 7,
+    name: "Victoria Nwafor",
+    username: "vnwafor",
+  },
+  {
+    id: 8,
+    name: "Segun Arinze",
+    username: "sarinez",
+  },
+];
